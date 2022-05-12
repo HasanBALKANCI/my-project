020ab0dc52f6a851a58d2c71834374e5eb11ebef
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-05-12 11:08:56 +0200

    /forth {id } was reviewed

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -15,10 +15,12 @@ def second():
 @app.route("/third/subthird")
 def third():
     return "<h2>This is the subpath of third page</h2>"
+    # you can see above h2 in web page 'localhost:2000/third/subthird
 
 @app.route("/forth/<string:id>")
 def forth(id):
     return f'Id of this page is {id}'
+    # it returns what you write in {...}, it is changeable.
 
 
 if __name__ == "__main__":
